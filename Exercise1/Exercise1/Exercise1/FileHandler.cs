ad25b208f68ba9a4ea54b0580a4ac53a0578a6ee
Lars Karlqvist <le.karlqvist@gmail.com>
2026-04-28 15:15:29 +0200

    Update 20260428
    Save to file and read file at program start added.
    Comments addded.

--- Exercise1/Exercise1/Exercise1/FileHandler.cs
+++ Exercise1/Exercise1/Exercise1/FileHandler.cs
@@ -8,33 +8,94 @@ namespace Exercise1
 {
     internal static class FileHandler
     {
+        /// <summary>
+        /// Method to add a person to the list, it will ask the user for the name, birth year and hourly rate of the person and then create a new Employee object and add it to the list.
+        /// </summary>
+        /// <param name="list">The list of employees to which the new person will be added.</param>
+        /// <returns>Returns true if the person was successfully added, otherwise false.</returns>
         internal static bool AddPerson(List<Employee> list)
         {
-            Console.WriteLine("Enter name:");
-            var name = Console.ReadLine();
-            Console.WriteLine("Enter birth year:");
-            var born = Console.ReadLine();
-            Console.WriteLine("Enter hourly rate:");
-            var hourlyRate = Console.ReadLine();
-
-            Employee person = new Employee(name, born, hourlyRate);
-
-            if (person == null)
+            try
             {
-                return false;
+                Console.WriteLine("Enter name:");
+                var name = Console.ReadLine();
+                Console.WriteLine("Enter birth year:");
+                var born = Console.ReadLine();
+                Console.WriteLine("Enter hourly rate:");
+                var hourlyRate = Console.ReadLine();
+
+                Employee person = new Employee(name, born, hourlyRate);
+
+                if (person == null)
+                {
+                    return false;
+                }
+                list.Add(person);
             }
-            list.Add(person);
+            catch (Exception ex)
+            {
+                Console.WriteLine("Somthing went wrong, try again!\n" + ex);
+            }
             return true;
         }
-
+        /// <summary>
+        /// Method to list people in the list, it will iterate through the list of employees and print their name, birth year and hourly rate to the console.
+        /// </summary>
+        /// <param name="list">The list of employees to be displayed.</param>
         internal static void ShowPeople(List<Employee> list)
         {
             foreach (var person in list)
             {
-                Console.WriteLine($"Name: {person.Name}, Born: {person.Born}, Hourly Rate: {person.HourlyRate}");
+                Console.WriteLine($" Name: {person.Name},\n Born: {person.Born},\n Hourly Rate: {person.HourlyRate}\n");
             }
 
         }
+        /// <summary>
+        /// Method to save people to a file, it will create a new file if it doesn't exist and then write the name, birth year and hourly rate of each employee in the list to the file in a specific format.
+        /// </summary>
+        /// <param name="list">The list of employees to be saved.</param>
+        /// <param name="filePath">The file path where the employees will be saved.</param>
+        internal static void SavePeople(List<Employee> list, string filePath)
+        {
+            if(!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (var person in list)
+            {
+                sb.AppendLine($"Name:{person.Name},Born:{person.Born},HourlyRate:{person.HourlyRate}");
+            }
+            File.WriteAllText(filePath, sb.ToString());
+        }
+        /// <summary>
+        /// Method to load people from a file, it will read the file line by line and split the line into parts to extract the name, birth year and hourly rate of each employee and then create a new Employee object and add it to a list which is returned at the end.
+        /// </summary>
+        /// <param name="filePath">The file path from which the employees will be loaded.</param>
+        /// <returns>A list of employees loaded from the file.</returns>
+        public static List<Employee> LoadPeople( string filePath)
+        {
+            List<Employee> liststring = new List<Employee>();
 
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+
+            var lines = File.ReadAllLines(filePath);
+            liststring.Clear();
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(',');
+                var name = parts[0].Split(':')[1];
+                var born = parts[1].Split(':')[1];
+                var hourlyRate = parts[2].Split(':')[1];
+                Employee person = new Employee(name, born, hourlyRate);
+                liststring.Add(person);
+            }
+            
+        return liststring;
+        }
     }
 }
--- Exercise1/Exercise1/Exercise1/Program.cs
+++ Exercise1/Exercise1/Exercise1/Program.cs
@@ -6,8 +6,19 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
+            /// <summary>
+            /// The main entry point of the program. It initializes the list of employees,
+            /// loads existing employees from a file if available, and provides a menu
+            /// for the user to add, show, save, or exit the program.
+            /// </summary>
             List<Employee> list = new List<Employee>();
+            Console.WriteLine(Environment.GetCommandLineArgs()[0].Replace("Exercise1.dll", ""));
+            string installationPath = Environment.GetCommandLineArgs()[0].Replace("Exercise1.dll", "")+"dbfile.txt";
 
+            if(File.Exists(installationPath)&& new FileInfo(installationPath).Length > 0) {
+                list = FileHandler.LoadPeople(installationPath);
+            }
+            
             do
             {
                 Console.WriteLine(GetMenu());
@@ -24,7 +35,14 @@ namespace Exercise1
                 }
                 else if (input == "3")
                 {
+                    Console.WriteLine("Save people");
+                    FileHandler.SavePeople(list, installationPath);
+                    break;
+                }
+                else if (input == "4")
+                {
                     Console.WriteLine("Exit");
+                    FileHandler.SavePeople(list, installationPath);
                     break;
                 }
             } while (true);
@@ -32,10 +50,14 @@ namespace Exercise1
             Console.ReadLine();
         }
 
-
+        /// <summary>
+        /// The GetMenu method returns a string that represents the menu options for the user. 
+        /// It includes options to add a person, show people, save people, and exit the program.
+        /// </summary>
+        /// <returns></returns>
         internal static String GetMenu()
         {
-            return "1. Add person\n2. Show people\n3. Exit\n";
+            return "1. Add person\n2. Show people\n3. Save people\n4. Exit\n";
         }
     }
 }
