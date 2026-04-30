156ab5b07d35da4c1e7c87084bf5c95410d76e25
KarlqvistLars <le.karlqvist@gmail.com>
2026-04-30 14:23:18 +0200

    Added function on single ticket buy

--- Exercise2/Exercise2/Exercise2/Program.cs
+++ Exercise2/Exercise2/Exercise2/Program.cs
@@ -30,7 +30,6 @@ namespace Exercise2
                 }
             } while (true);
         }
-
         /// <summary>
         /// Menyer för biobiljetter
         /// </summary>
@@ -43,7 +42,6 @@ namespace Exercise2
             Console.WriteLine("1. Köp enkelbiljett.\n2. Köp biljett till sällskap.\n0. Avsluta\n");
             Console.Write("Ange menyval: ");
         }
-
         internal static void PrintMenuEnkelbiljett()
         {
             Console.Clear();
@@ -52,11 +50,10 @@ namespace Exercise2
             Console.WriteLine("\u001b[4mBiljettpriser\u001b[0m");
             Console.WriteLine("Vuxen: \t\t\t120kr");
             Console.WriteLine("Ungdom(under 20 år):\t80kr");
-            Console.WriteLine("Pensionär(över 64år):\t120kr");
+            Console.WriteLine("Pensionär(över 64år):\t90kr");
             Console.Write("Ange er ålder: ");
             ExecuteTicketTransaction(Console.ReadLine(), 0);
         }
-
         internal static void PrintMenuSamlingsbiljett()
         {
             Console.Clear();
@@ -65,15 +62,53 @@ namespace Exercise2
             Console.WriteLine("\u001b[4mBiljettpriser\u001b[0m");
             Console.WriteLine("Vuxen: \t\t\t120kr");
             Console.WriteLine("Ungdom(under 20 år):\t80kr");
-            Console.WriteLine("Pensionär(över 64år):\t120kr");
+            Console.WriteLine("Pensionär(över 64år):\t90kr");
             Console.Write("Ange antal: ");
             ExecuteTicketTransaction(Console.ReadLine(), 1);
         }
-
         internal static void ExecuteTicketTransaction(string? input, int type)
         {
             Console.WriteLine("Executing ticket transaction..."+ input + " " + type);
-        }
+            switch (type)
+            {
+                case 0:
+                    BuySingleTicket(input);
+                    break;
+                case 1:
+                    BuyCompanyTicket(input);
+                    break;
+                default:
+                    break;
+            }
 
+            Console.ReadLine();
+            PrintHeadMenu();
+        }
+        private static void BuySingleTicket(string? input)
+        {
+            if (int.TryParse(input, out int age))
+            {
+                if (age < 20)
+                {
+                    Console.WriteLine("Ungdomsbiljett köpt för 80kr.");
+                }
+                else if (age > 64)
+                {
+                    Console.WriteLine("Pensionärsbiljett köpt för 90kr.");
+                }
+                else
+                {
+                    Console.WriteLine("Vuxenbiljett köpt för 120kr.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ogiltig ålder. Försök igen.");
+            }
+        }
+        private static void BuyCompanyTicket(string? input)
+        {
+            throw new NotImplementedException();
+        }
     }
  }
