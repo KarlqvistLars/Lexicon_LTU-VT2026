46d930d99e21441610c9260c30bac55ce85b7312
KarlqvistLars <le.karlqvist@gmail.com>
2026-05-06 17:49:24 +0200

    Error flagga tillagt getterfunktionerna för att hanter avvikelserna

--- Exercise3/Exercise3/Exercise3/Program.cs
+++ Exercise3/Exercise3/Exercise3/Program.cs
@@ -4,42 +4,50 @@
     {
         static void Main(string[] args)
         {
-         if(false)
-            {
-                var lines = 5;
-                var persons = new List<Person>();
-                for (int i = 0; i < lines; i++)
-                {
-    #pragma warning disable CS8602 // Dereference of a possibly null reference.
-                    var cmdArgs = Console.ReadLine().Split(' ');
-    #pragma warning restore CS8602 // Dereference of a possibly null reference.
-                    Console.WriteLine(cmdArgs[0]);
-                    Person person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
-                    persons.Add(person);
-                }
-
-                persons.OrderBy(p => p.FirstName)
-                    .ThenBy(p => p.Age)
-                    .ToList()
-                    .ForEach(p => Console.WriteLine(p.ToString()));
-            //StartUp.ReadLines(persons, lines);
-        }else {
-                var lines = int.Parse(Console.ReadLine());
-                var persons = new List<Person>();
-                for (int i = 0; i < lines; i++)
-                {
-                    var cmdArgs = Console.ReadLine().Split();
-                    var person = new Person(cmdArgs[0],
-                    cmdArgs[1],
-                   int.Parse(cmdArgs[2]),
-                   decimal.Parse(cmdArgs[3]));
-                    persons.Add(person);
-                }
-                var bonus = decimal.Parse(Console.ReadLine());
-                persons.ForEach(p => p.IncreaseSalary(bonus));
-                persons.ForEach(p => Console.WriteLine(p.ToString()));
+            int lines = 0;
+            while (lines == 0)
+            {   
+                Console.Write("Ange antal personer att lägga till: ");
+                lines = int.TryParse(Console.ReadLine(), out int result) ? result : 0;
+                lines = lines <= 0 ? 0 : lines ;
             }
-
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                if (cmdArgs.Length == 3)
+                {
+                    var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                    if (!person.ErrFlag)
+                    {   
+                        persons.Add(person);
+                    }
+                    else
+                    {
+                        i--;                    
+                    }
+                }
+                else if (cmdArgs.Length == 4)
+                {
+                    var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), decimal.Parse(cmdArgs[3]));
+                    if (!person.ErrFlag)
+                    {
+                        persons.Add(person);
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Felaktigt antal argument. Förväntat 3 eller 4 argument.");
+                    i--;
+                }
+            }
+            var bonus = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(bonus));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
         }
     }
 }
